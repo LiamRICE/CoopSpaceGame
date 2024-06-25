feaf4f3f9688994a85d72d01dc22929c36d09ff6
Rex1549 <alex.rice1549@gmail.com>
2024-06-25 15:51:26 +0200

    Added test scene and flammable property object

---
+++ scenes/objects/Properties/flammable.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@export var is_burning :=false:
+	set(value):
+		is_burning = value
+@export var is_spreading :=false:
+	set(value):
+		is_spreading = value
+
+
+func _spread(radius :float):
+	pass
